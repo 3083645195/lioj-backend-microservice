842a72acf1802fe6d53cf9b83539c7b11e0e0c66
立 <3083645195@qq.com>
2024-05-21 11:02:32 +0800

    Initial commit

---
+++ lioj-backend-model/src/main/java/com/zhaoli/liojbackendmodel/model/dto/question/JudgeConfig.java
@@ -0,0 +1,24 @@
+package com.zhaoli.liojbackendmodel.model.dto.question;
+
+import lombok.Data;
+
+/**
+ * 判题配置
+ *
+ * @author 赵立
+ */
+@Data
+public class JudgeConfig {
+    /**
+     * 时间限制(ms)
+     */
+    private int timeLimit;
+    /**
+     * 内存限制(KB)
+     */
+    private int memoryLimit;
+    /**
+     * 堆栈限制(KB)
+     */
+    private int stackLimit;
+}
