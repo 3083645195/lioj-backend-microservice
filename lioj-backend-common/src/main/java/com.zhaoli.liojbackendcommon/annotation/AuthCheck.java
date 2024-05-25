a3e9d9689185fdb4c83158f1babb6087e00a87fc
zhaoli <119091025+3083645195@users.noreply.github.com>
2024-05-25 14:40:47 +0800

    perf

---
+++ lioj-backend-common/src/main/java/com.zhaoli.liojbackendcommon/annotation/AuthCheck.java
@@ -0,0 +1,24 @@
+package com.zhaoli.liojbackendcommon.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 权限校验
+ *
+ * @author zhaoli
+ * @from 
+ */
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface AuthCheck {
+
+    /**
+     * 必须有某个角色
+     */
+    String mustRole() default "";
+
+}
+
